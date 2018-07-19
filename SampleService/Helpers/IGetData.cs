b0fc69abf15c34299a6d39f7e2330ca0fb52da8a
Mike Andrade <mikea94@hotmail.com>
2018-07-19 08:59:33 -0400

    Some simple MOQ mocks in Unit tests for GetData interface

---
+++ SampleService/Helpers/IGetData.cs
@@ -0,0 +1,16 @@
+﻿using SampleService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleService.Helpers
+{
+    public interface IGetData
+    {
+        List<City> GetSampleCities();
+        List<Animal> GetSampleWildlife();
+
+    }
+}
---
+++ SampleService/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿namespace SampleService.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Animal
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+
+        public string Alias { get; set; }
+        
+        public string Habitat { get; set; }
+
+    }
+}
